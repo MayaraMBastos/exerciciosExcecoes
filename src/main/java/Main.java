9512957ccec3ca9c763f5cfddafaa85c86b357e4
Mayara Martinello Bastos <maybastos2021@gmail.com>
2024-11-09 17:54:42 -0300

    erros aritimeticos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         try {
             int num1 = sc.nextInt();
+            sc.nextLine();
             int num2 = sc.nextInt();
+            sc.nextLine();
             erroCalculo(num1, num2);
         }catch (InputMismatchException e){
             System.out.println("Erro de tipo de entrada " +e.getMessage());
@@ -15,13 +17,11 @@ public class Main {
 
     }
 
-    public static void erroCalculo(int num1, int num2) {
+    public static void divisaoCalculo(int num1, int num2) {
         try {
             int divisaoInteira = num1 / num2;
             System.out.println(divisaoInteira);
-        }catch (ArithmeticException e){
-            System.out.println("Erro de calculo Aritimetico: " + e.getMessage());
-        }catch (InputMismatchException e){
+        }catch (ArithmeticException e) {
             System.out.println("Erro de calculo Aritimetico: " + e.getMessage());
         }
 
